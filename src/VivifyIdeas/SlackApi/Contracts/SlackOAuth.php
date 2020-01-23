5594d94831e0c36a5776ab4ff561eeb9130c669f
Stefan Ugrinov <fiste_94@hotmail.com>
2020-01-23 22:24:28 +0100

    Add missing method in oauth, add PhpDocs

--- src/VivifyIdeas/SlackApi/Contracts/SlackOAuth.php
+++ src/VivifyIdeas/SlackApi/Contracts/SlackOAuth.php
@@ -4,5 +4,21 @@ namespace VivifyIdeas\SlackApi\Contracts;
 
 interface SlackOAuth
 {
-    public function access($code, $options = []);
+    /**
+     * Exchanges a temporary OAuth verifier code for a workspace token.
+     *
+     * @param string $client_id
+     * @param string $client_secret
+     * @param string $code
+     * @param array $options
+     */
+    public function token(string $client_id, string $client_secret, string $code, array $options = []);
+
+    /**
+     * Exchanges a temporary OAuth verifier code for an access token.
+     *
+     * @param string $code
+     * @param array $options
+     */
+    public function access(string $code, array $options = []);
 }
--- src/VivifyIdeas/SlackApi/Methods/OAuth.php
+++ src/VivifyIdeas/SlackApi/Methods/OAuth.php
@@ -9,13 +9,28 @@ class OAuth extends SlackMethod implements SlackOAuth
     protected $methodsGroup = 'oauth.';
 
     /**
+     * Exchanges a temporary OAuth verifier code for a workspace token.
+     *
+     * @param string $client_id - Issued when you created your application.
+     * @param string $client_secret - Issued when you created your application.
+     * @param string $code - The code param returned via the OAuth callback.
+     * @param array $options ['redirect_uri' => 'https://...', single_channel => true/false]
+     *
+     * @return array
+     */
+    public function token(string $client_id, string $client_secret, string $code, array $options = [])
+    {
+        $requiredParams = compact('client_id', 'client_secret', 'code');
+        return $this->method('token', array_merge($requiredParams, $options));
+    }
+    /**
      * 	Exchanges a temporary OAuth verifier code for an access token.
      * @param string $code
      * @param array  $options ['redirect_uri' => 'https://...', 'client_id' => '...', 'client_secret' => '...']
      *
      * @return array
      */
-    public function access($code, $options = [])
+    public function access(string $code, array $options = [])
     {
         return $this->method('access', array_merge([
             'code' => $code,
